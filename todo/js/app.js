1a17aa9b4aa86b8a9bfaff16f04d9b38ef5a2d4f
kjy3408 <wodud2312@naver.com>
2023-03-16 17:51:26 +0900

    ToDoList 구현 - aside 완료 - Information 완료 - Todo 구현중

--- todo/js/app.js
+++ todo/js/app.js
@@ -3,4 +3,15 @@ window.onload = () => {
     AsideEvent.getInstance().addEventMainChange();
     InformationEvent.getInstance().addEventPhotoChangeClick();
     InformationEvent.getInstance().addEventPhotoChange();
+    // InformationEvent.getInstance().addEventWriteAboutMe();
+    // InformationEvent.getInstance().addEventWriteIntroduction();
+    InformationEvent.getInstance().addEventAboutMeModifyClick();
+    InformationEvent.getInstance().addEventAboutMeSaveClick();
+    InformationEvent.getInstance().addEventIntroduceModifyClick();
+    InformationEvent.getInstance().addEventIntroduceSaveClick();
+    ToDoEvent.getInstance().addEventAddTodoClick();
+    ToDoEvent.getInstance().addEventAddTodoKeyUp();
+    TodoService.getInstance();
+    TodoService.getInstance().deleteTodoList();
+    
 }
--- todo/js/information.js
+++ todo/js/information.js
@@ -21,6 +21,100 @@ class InformationEvent {
             fileService.getInstance().changePhoto();
         } 
     }
+
+    addEventAboutMeModifyClick() {
+        const aboutMeModifyButton = document.querySelector(".m-aboutme")
+        aboutMeModifyButton.onclick = () => {
+            const aboutMeSaveButton = document.querySelector(".s-aboutme");
+            aboutMeSaveButton.classList.remove("button-hidden");
+            aboutMeModifyButton.classList.add("button-hidden");
+           
+            const infoInputContainers = document.querySelectorAll(".info-input-container");
+            infoInputContainers.forEach(infoInputContainer => {
+                infoInputContainer.querySelector(".info-input").disabled = false;
+            });            
+        }
+    }
+    
+    addEventAboutMeSaveClick() {
+        const aboutMeSaveButton = document.querySelector(".s-aboutme");
+        aboutMeSaveButton.onclick = () => {
+            const aboutMeModifyButton = document.querySelector(".m-aboutme")
+            aboutMeSaveButton.classList.add("button-hidden");
+            aboutMeModifyButton.classList.remove("button-hidden"); 
+           
+            const infoInputContainers = document.querySelectorAll(".info-input-container");
+            infoInputContainers.forEach(infoInputContainer => {
+                infoInputContainer.querySelector(".info-input").disabled = true;
+            });            
+        }
+    }
+
+    addEventIntroduceModifyClick() {
+        const introduceModifyButton = document.querySelector(".m-introduce")
+        introduceModifyButton.onclick = () => {
+            const introduceSaveButton = document.querySelector(".s-introduce");
+            introduceSaveButton.classList.remove("button-hidden");
+            introduceModifyButton.classList.add("button-hidden");
+            const introduceInput = document.querySelector(".introduce-input");
+            introduceInput.disabled = false;
+        }
+    }
+    
+    addEventIntroduceSaveClick() {
+        const introduceSaveButton = document.querySelector(".s-introduce");
+        introduceSaveButton.onclick = () => {
+            const introduceModifyButton = document.querySelector(".m-introduce")
+            introduceSaveButton.classList.add("button-hidden");
+            introduceModifyButton.classList.remove("button-hidden"); 
+            const introduceInput = document.querySelector(".introduce-input");
+            introduceInput.disabled = true;
+                     
+        }
+    }
+
+    // 위아래 암거나 써도됨
+
+    // addEventWriteAboutMe() {
+    //     const mAboutMe = document.querySelector(".m-aboutme");
+    //     const infoInputs = document.querySelectorAll(".info-input");
+    //     const sAboutMe = documecorrectionnt.querySelector(".s-aboutme");
+       
+    //     mAboutMe.onclick = () => {
+    //         infoInputs.forEach(infoInput => {
+    //             infoInput.disabled = false;
+    //         });  
+    //         sAboutMe.classList.remove("button-hidden");
+    //         mAboutMe.classList.add("button-hidden");
+    //     }
+        
+    //     sAboutMe.onclick = () => {
+    //         infoInputs.forEach(infoInput => {
+    //             infoInput.disabled = true;
+    //         });  
+    //         sAboutMe.classList.add("button-hidden");
+    //         mAboutMe.classList.remove("button-hidden");
+    //     }
+    // }
+
+    // addEventWriteIntroduction() {
+    //     const mIntroduce = document.querySelector(".m-introduce");
+    //     const introduceInput = document.querySelector(".introduce-input");
+    //     const sIntroduce = document.querySelector(".s-introduce");
+
+    //     mIntroduce.onclick = () => {
+    //         introduceInput.disabled = false;
+    //         sIntroduce.classList.remove("button-hidden");
+    //         mIntroduce.classList.add("button-hidden");
+    //     }
+        
+    //     sIntroduce.onclick = () => {
+    //         introduceInput.disabled = true;
+    //         sIntroduce.classList.add("button-hidden");
+    //         mIntroduce.classList.remove("button-hidden");
+    //     }
+    // }
+
 }
 
 class fileService {
